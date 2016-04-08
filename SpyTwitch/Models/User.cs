e78ffb6bd01e264e51026a9be58a1200bd682d2d
xerq <xerq@openmailbox.org>
2016-04-08 20:24:53 +0200

    beautify code

---
+++ SpyTwitch/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TwitchSpy
+{
+	public class User
+	{
+		public string name;
+		public List<WatchLog> watchingList;
+		public List<string> follows;
+
+		public User (string name)
+		{
+			this.name = name;
+			watchingList = new List<WatchLog> ();
+			follows = new List<string> ();
+		}
+	}
+}
+
---
+++ SpyTwitch/Models/WatchLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TwitchSpy
+{
+	public struct WatchLog
+	{
+		public string channelName;
+		public DateTime when;
+
+		public WatchLog (string channelName, DateTime when)
+		{
+			this.channelName = channelName;
+			this.when = when;
+		}
+	}
+}
+
